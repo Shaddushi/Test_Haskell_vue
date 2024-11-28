4aa3a376c6c50171107541effcf868d2d879fc67
Shaddushi <duportail.honore@gmail.com>
2024-11-28 21:21:50 +0100

    ajout de la partie web - fin

---
+++ Vue_frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  const message = ref(''); 
+  onMounted(async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/route');
+      message.value = response.data.message; 
+    } catch (error) {
+      console.error('Erreur lors de la récupération du message :', error);
+      message.value = 'Erreur lors de la connexion au serveur.';
+    }
+  });
+
+</script>
---
+++ Vue_frontend/src/components/composant_test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+      <button @click="fetchMessage">Recharger le message</button>
+    </div>
+  </template>
+  
+  <script>
+
+  </script>
+  
